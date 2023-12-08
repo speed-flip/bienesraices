848539de51d0de7e4ffa6b0f6f7461a349ad2a80
Victor Torres <vtorresquintanilla0206@gmail.com>
2023-12-08 17:32:41 -0600

    fetch: signUp component

--- src/screens/auth/SignUp.js
+++ src/screens/auth/SignUp.js
@@ -1,15 +1,45 @@
 import { useState } from 'react';
 
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, ScrollView } from 'react-native';
 import { TextInput, Button, Text } from 'react-native-paper';
+import AdminClient from '../../config/AdminClient';
+import CustomAlert from '../../components/CustomAlert';
 
 const SignUp = ({ navigation }) => {
-  const [nombre, setNombre] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [alerta, setAlerta] = useState({ msg: '', error: false });
 
   async function handleSubmit() {
-    console.log({ nombre, email, password });
+    const body = {
+      name,
+      username: 'user',
+      email,
+      password,
+    }
+
+    try {
+      setLoading(true);
+      const { data } = await AdminClient.post('/auth/signup', body);
+      setAlerta({
+        msg: data.msg ?? 'no hay msg en rsp',
+      });
+      console.log(data);
+      setAlerta({
+        msg: data.errorResponse ? `${data.errorResponse.msg} ${data.errorResponse.details}` : data.msg,
+        error: data.errorResponse ? true : false,
+      });
+    } catch (error) {
+      console.log(error);
+      setAlerta({
+        msg: error.message,
+        error: true,
+      });
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -18,7 +48,7 @@ const SignUp = ({ navigation }) => {
         source={require('../../../assets/img-auth.jpg')}
         style={styles.imagen}
       />
-      <View style={styles.formulario}>
+      <ScrollView style={styles.formulario}>
         <View style={{ marginBottom: 20 }}>
           <Text variant='headlineSmall' style={{ fontWeight: 'bold' }}>
             Registrarse
@@ -33,8 +63,8 @@ const SignUp = ({ navigation }) => {
           label="Nombre"
           placeholder="Jane Doe"
           style={{ marginBottom: 5 }}
-          value={nombre}
-          onChangeText={setNombre}
+          value={name}
+          onChangeText={setName}
         />
         <TextInput
           mode="outlined"
@@ -52,6 +82,7 @@ const SignUp = ({ navigation }) => {
           secureTextEntry={true}
           value={password}
           onChangeText={setPassword}
+          right={<TextInput.Icon icon="eye" />}
         />
 
         <Button
@@ -65,10 +96,25 @@ const SignUp = ({ navigation }) => {
           mode='contained'
           style={{ marginVertical: 15 }}
           onPress={handleSubmit}
+          loading={loading}
+          disabled={loading}
         >
           Crear Cuenta
         </Button>
-      </View>
+      </ScrollView>
+
+      <CustomAlert
+        alerta={alerta.msg ? true : false}
+        title={alerta.error ? 'Error' : ''}
+        content={alerta.msg}
+        onPress={() => {
+          if (alerta.error) {
+            setAlerta(false);
+          } else {
+            navigation.navigate('Login');
+          }
+        }}
+      />
     </View>
   );
 }
